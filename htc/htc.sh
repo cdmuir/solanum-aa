4dc02a8c31ba9d40c09f3a245542048c13621d48
cdmuir <chrisdmuir@gmail.com>
2024-02-27 15:11:44 -0600

    added htc folder

---
+++ htc/htc.sh
@@ -0,0 +1,62 @@
+# This script is not intended to be executed all at once.
+# Rather, it records steps needed to run Stan models on HTC
+
+# Resources:
+# * How to log in, upload data:
+# https://chtc.cs.wisc.edu/uw-research-computing/connecting
+# * How to prepare and submit jobs:
+# https://chtc.cs.wisc.edu/uw-research-computing/helloworld
+# How to run R jobs: https://chtc.cs.wisc.edu/uw-research-computing/r-jobs
+
+# Preliminary: build R packages and Stan
+# 
+## Locally, transfer build jobs to submite node
+scp htc/build-r.sub cdmuir@submit2.chtc.wisc.edu:/home/cdmuir/solanum-aa
+scp htc/build-stan.sub cdmuir@submit2.chtc.wisc.edu:/home/cdmuir/solanum-aa
+
+## Login to submit node
+ssh cdmuir@submit2.chtc.wisc.edu
+
+## On submit node
+condor_submit -i build-r.sub
+# install packages, quit r
+tar -czf packages.tar.gz packages/
+# quit interactive job
+# R packages are ready
+condor_submit -i build-stan.sub
+tar -xzf cmdstan-2.34.1.tar.gz
+cd cmdstan-2.34.1
+# MAY NEED TO DO CONFIGURE STEP HERE?
+# ./configure --prefix=$_CONDOR_SCRATCH_DIR/cmdstan
+make build
+cd ..
+tar -czf cmdstan-2.34.1.tar.gz cmdstan-2.34.1/
+
+
+
+
+# 1. Compress local directory before transfering
+tar --exclude='packages.tar.gz' -czf solanum-aa.tar.gz htc/
+
+# 2. Transfer files before logging in using scp
+scp solanum-aa.tar.gz cdmuir@submit2.chtc.wisc.edu:/home/cdmuir
+
+# 3. Remove tarball locally
+rm solanum-aa.tar.gz
+
+# 4. Login to submit node
+
+# 5. Untar on submit node
+tar -xzf solanum-aa.tar.gz -C solanum-aa
+
+# 6. Remove tarball from submit node
+rm solanum-aa.tar.gz
+
+# 7. Submit jobs
+condor_submit solanum-aa/htc/fit_0001.sub # 19164174
+condor_submit solanum-aa/htc/fit_0002.sub # 19164175
+condor_submit solanum-aa/htc/fit_0003.sub # 19164176
+
+
+# check status
+condor_q 19164174
