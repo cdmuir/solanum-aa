825017c83184550bbbdb98d84d68faaab854c019
cdmuir <chrisdmuir@gmail.com>
2024-03-08 08:32:29 -0600

    working on integration model

--- r/scratch-integration2.R
+++ r/scratch-integration2.R
@@ -20,54 +20,97 @@ rh_curves = read_rds("data/thinned_rh_curves.rds") |>
   filter(acc == "LA2172") |>
   mutate(
     log_gsw = log(gsw),
-    scaled_log_gsw = (log_gsw - mean(log_gsw)) / sd(log_gsw) 
-  )
+    scaled_log_gsw = (log_gsw - mean(log_gsw)) / sd(log_gsw)
+  ) |>
+  dplyr::select(
+    A,
+    scaled_log_gsw,
+    curve,
+    id,
+    leaf_type,
+    light_intensity,
+    lightintensity_x_id
+  ) 
 
 # check
 mean(rh_curves$scaled_log_gsw)
 sd(rh_curves$scaled_log_gsw)
 
-# WORKING HERE
-# ALMOST THERE - JUST NEED TO COMPOSE CURVE BEFORE TRANSFERRING TO LIST
-rh_curves |>
-  # select variables needed for Stan
-  dplyr::select(
-    A,
-    scaled_log_gsw,
-    curve,
-    id,
-    leaf_type,
-    light_intensity,
-    lightintensity_x_id
-  ) |>
-  summarize(
-    curve = first(curve),
-    .by = c(light_intensity, id)
-  )
 
-
+# Compose data for Stan
 stan_rh_curves = rh_curves |>
-  # select variables needed for Stan
-  dplyr::select(
-    A,
-    scaled_log_gsw,
-    curve,
-    id,
-    leaf_type,
-    light_intensity,
-    lightintensity_x_id
-  ) |>
   compose_data()
 
 # Manual changes
+stan_rh_curves = c(
+  stan_rh_curves,
+  
+  ## Mapping of lightintensity_x_id to curve
+  rh_curves |>
+  mutate(
+    across(c("lightintensity_x_id", "curve"), \(.x) as.numeric(as.factor(.x)))
+  ) |>
+  summarize(
+    curve = first(curve),
+    .by = c(lightintensity_x_id, leaf_type)
+  ) |>
+  pivot_wider(names_from = "leaf_type", values_from = "curve") |>
+  arrange(lightintensity_x_id) |>
+  dplyr::select(amphi, pseudohypo) |>
+  as.list(),
+
+  ## min and max scaled_log_gsw by curve
+  rh_curves |>
+  mutate(across(c("curve"), \(.x) as.numeric(as.factor(.x)))) |>
+    summarize(
+      min_scaled_log_gsw = min(scaled_log_gsw),
+      max_scaled_log_gsw = max(scaled_log_gsw),
+      .by = c(curve)
+    ) |>
+    arrange(curve) |>
+    dplyr::select(min_scaled_log_gsw, max_scaled_log_gsw) |>
+    as.list(),
+  
+  rh_curves |>
+    mutate(across(c("curve", "leaf_type"), \(.x) as.numeric(as.factor(.x)))) |>
+    summarize(
+      leaf_type = first(leaf_type),
+      .by = c(curve)
+    ) |>
+    arrange(curve) |>
+    dplyr::select(tmp = leaf_type) |>
+    as.list()
+  
+)
+
 stan_rh_curves$n_pts = rh_curves |>
   summarise(n = n(), .by = "curve") |>
   pull(n)
 stan_rh_curves$d1 = 0
 stan_rh_curves$d2 = 0L
 
+  
 # Fit
 m = cmdstan_model("stan/solanum-aa4.stan", dir = "stan/bin")
+
+init = list(
+  b0 = stan_rh_curves$tmp,
+  b1 = rep(0, stan_rh_curves$n_curve),
+  b2 = rep(0, stan_rh_curves$n_curve),
+  mu_aa = 0,
+  sigma_aa = 0.1,
+  aa = rep(0, stan_rh_curves$n_lightintensity_x_id)
+)
+
+init = rh_curves |>
+  mutate(across(c("curve"), \(.x) as.numeric(as.factor(.x)))) |>
+  split(~ curve) |>
+  map_dfr(\(.x) {
+    fit = lm(A ~ scaled_log_gsw + I(scaled_log_gsw ^ 2), data = .x)
+    b = unname(coef(fit))
+    tibble(b0 = b[1], b1 = b[2], b2 = b[3])
+  }) |>
+  as.list()
 
 fit = m$sample(
   data = stan_rh_curves,
@@ -75,7 +118,19 @@ fit = m$sample(
   parallel_chains = 2L,
   iter_warmup = 1e3,
   iter_sampling = 1e3,
-  refresh = 2e1
+  refresh = 2e1,
+  init = list(init, init)
 )
 
 fit$profiles()
+tmp = fit$summary("aa") |>
+  dplyr::select(variable, median, q5, q95) |>
+  arrange(median) |>
+  mutate(n = row_number())
+  print(n = 40)
+
+ggplot(tmp, aes(n, median, ymin = q5, ymax = q95)) +
+  geom_pointinterval() +
+  geom_hline(yintercept = 0)
+
+fit1 = fit
--- stan/solanum-aa4.stan
+++ stan/solanum-aa4.stan
@@ -29,6 +29,14 @@ data {
   vector[n] A;
   vector[n] scaled_log_gsw;
 
+  // index of amphi and pseudohypo curves for each light_intensity x id combination
+  array[n_lightintensity_x_id] int<lower=0,upper=n_curve> amphi;
+  array[n_lightintensity_x_id] int<lower=0,upper=n_curve> pseudohypo;
+
+  // min and max scaled_log_gsw by curve
+  array[n_curve] real min_scaled_log_gsw;
+  array[n_curve] real max_scaled_log_gsw;
+  
   array[1] real d1;
   array[1] int d2;
 
@@ -36,12 +44,18 @@ data {
 parameters {
   
   // quadratic regression coefficients
-  vector<lower=0>[n_curve] b0;
-  vector<lower=0>[n_curve] b1;
-  vector<lower=0>[n_curve] b2;
+  vector[n_curve] b0;
+  vector[n_curve] b1;
+  vector[n_curve] b2;
 
   real<lower=0> sigma;
 
+  // vector[n_lightintensity_x_id] aa;
+  
+  // hyperparameters
+  real mu_aa;
+  real<lower=0> sigma_aa;
+  
 }
 model {
   
@@ -52,8 +66,51 @@ model {
     b2 ~ normal(0, 10);
   
     sigma ~ normal(0, 1); 
+    
+    mu_aa ~ normal(0, 1);
+    sigma_aa ~ normal(0, 1);
+    
   }
   
+      profile("Estimate AA") {
+  // Estimate AA
+  
+  for (i in 1:n_lightintensity_x_id) {
+    
+    real aa_i;
+    int amphi_curve; 
+    int pseudohypo_curve;
+    
+    real a; // min of amphi curve
+    real b; // max of hypo
+    array[6] real theta; // parameters
+    
+    amphi_curve = amphi[i];
+    pseudohypo_curve = pseudohypo[i];
+    
+    a = min_scaled_log_gsw[amphi_curve];
+    b = max_scaled_log_gsw[pseudohypo_curve];
+    
+    theta[1] = b0[amphi_curve];      // b0_amphi;
+    theta[2] = b1[amphi_curve];      // b1_amphi;
+    theta[3] = b2[amphi_curve];      // b2_amphi;
+    theta[4] = b0[pseudohypo_curve]; // b0_hypo;
+    theta[5] = b1[pseudohypo_curve]; // b1_hypo;
+    theta[6] = b2[pseudohypo_curve]; // b2_hypo;
+    
+    // print("amphi_curve: ", amphi_curve);
+    // print("pseudohypo_curve: ", pseudohypo_curve);
+    // print("a: ", a);
+    // print("b: ", b);
+    // print("theta: ", theta);
+    aa_i = integrate_1d(aa_int, a, b, theta, d1, d2);
+    // print(i, ", aa[i]: ", aa[i]);
+
+    target += normal_lpdf(aa_i | mu_aa, sigma_aa);
+
+  }
+  }
+
   profile("likelihood") {
   // likelihood ----
     for (i in 1:n) {
@@ -67,29 +124,4 @@ model {
     }
   }
   
-}
-generated quantities {
-  
-  # Calculate AA
-  vector[n_lightintensity_x_id] aa;
-  
-  for (i in 1:n_lightintensity_x_id) {
-    
-    real a; // min of amphi curve
-    real b; // max of hypo
-    array[6] real theta; // parameters
-    
-    a = min(scaled_log_gsw[???]);
-    b = max(scaled_log_gsw[???]);
-    
-    theta[1] = b0[???] // b0_amphi;
-    theta[2] = b1[???] // b1_amphi;
-    theta[3] = b2[???] // b2_amphi;
-    theta[4] = b0[???] // b0_hypo;
-    theta[5] = b1[???] // b1_hypo;
-    theta[6] = b2[???] // b2_hypo;
-    
-    aa[i] = integrate_1d(aa_int, a, b, theta, d1, d2);
-
-  }
 }
