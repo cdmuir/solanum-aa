eded8b95e9e267750959c2730c33c19a621e53a7
cdmuir <chrisdmuir@gmail.com>
2024-03-13 10:58:11 -0500

    added htc files for solanum-aa1

---
+++ htc/fit_aa1.R
@@ -0,0 +1,38 @@
+library(cmdstanr)
+library(dplyr)
+library(purrr)
+library(readr)
+library(stringr)
+library(tibble)
+
+set_cmdstan_path("cmdstan-2.34.1")
+
+m = cmdstan_model("solanum-aa1.stan")
+
+rh_curves = read_rds("prepared_rh_curves.rds")
+stan_rh_curves = read_rds("stan_rh_curves.rds")
+
+init = rh_curves |>
+  mutate(across(c("curve"), \(.x) as.numeric(as.factor(.x)))) |>
+  split(~ curve) |>
+  map_dfr(\(.x) {
+    fit = lm(log(A) ~ scaled_log_gsw + I(scaled_log_gsw ^ 2), data = .x)
+    b = unname(coef(fit))
+    tibble(b0 = b[1], b1 = b[2], b2 = b[3])
+  }) |>
+  as.list()
+
+fit_aa1 = m$sample(
+  data = stan_rh_curves,
+  chains = 2L,
+  parallel_chains = 2L,
+  init = list(init, init),
+  seed = 898932814,
+  iter_warmup = 1e3,
+  iter_sampling = 1e3,
+  thin = 1e0
+  # max_treedepth = 12L,
+  # adapt_delta = 0.9
+)
+    
+fit_aa1$save_object("fit_aa1.rds")
