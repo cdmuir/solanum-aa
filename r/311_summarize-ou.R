0e6f7f83bfb7224e05eef8f71b89e6413ee39d2e
cdmuir <chrisdmuir@gmail.com>
2024-04-08 16:03:38 -0500

    added draft of tbl-parameters

---
+++ r/311_summarize-ou.R
@@ -0,0 +1,5 @@
+# Summarize OU parameters
+# Not sure what goes here, just leaving notes on how to map from {eta^2, rho^2} -> {simga^2, alpha} OU parameterization
+# 
+# alpha = rho ^ 2
+# sigma ^ 2 = 2 * eta ^ 2 * rho ^ 2
