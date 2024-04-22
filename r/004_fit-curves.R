a47f0ccd3246db013f3ce4f1276f77712436e726
Chris Muir <cdmuir@wisc.edu>
2024-04-22 18:20:31 -0500

    trying out one-by-one rh-curve fitting

---
+++ r/004_fit-curves.R
@@ -0,0 +1,40 @@
+# new approach: estimate AA for every curve individually
+source("r/header.R")
+
+rh_curves = read_rds("data/trimmed_rh_curves.rds")
+
+rh_curves |>
+  split(~ curve) |>
+  iwalk(\(df, curve_id) {
+    x = 1
+    ad = 0.8
+    n_divergent = Inf
+    while (n_divergent > 10) {
+      fit_curve = brm(
+        log_A ~ poly(log_gsw, 2, raw = TRUE) +
+          arma(
+            time = NA,
+            gr = NA,
+            p = 1,
+            q = 0,
+            cov = FALSE
+          ),
+        iter = x * 2000,
+        thin = x,
+        data = df,
+        chains = 4,
+        cores = 4,
+        backend = "cmdstanr",
+        control = list(adapt_delta = ad)
+      )
+      n_divergent = nuts_params(fit_curve) |>
+        subset(Parameter == "divergent__") |>
+        pull(Value) |>
+        sum()
+      x = x + 1
+      ad = min(ad * 1.1, 0.99)
+    }
+    
+    write_rds(fit_curve, paste0("objects/curve-fits/", curve_id, ".rds"))
+    
+  })
--- r/305_check-convergence.R
+++ r/305_check-convergence.R
@@ -1,5 +1,28 @@
 # Check model convergence (MIGHT NEED TO EDIT TO INCLUDE TERMS FOR RH CURVE TERMS)
+# This scripts needs some adjusting after models finish running
+
 source("r/header.R")
+
+# args = commandArgs(trailingOnly = TRUE)
+args = list("aa1", "0")
+
+m = args[1]
+chain = args[2]
+chkpt_path1 = glue("checkpoints/chkpt_folder_{m}_{chain}")
+
+# use this once model finishes running
+# draws = read_rds(draws, glue("objects/draws_{m}_{chain}.rds"))
+draws = combine_chkpt_draws1(path = chkpt_path1) # temporary version
+
+focal_pars = get_par_table(m) |>
+  dplyr::filter(type == "real") |>
+  pull(parameter)
+
+s = posterior::summarize_draws(draws)
+s |>
+  dplyr::filter(variable %in% focal_pars)
+
+par_summary = draws$summary(focal_pars)
 
 paste0("aa", 1:5) |>
   walk(\(model) {
