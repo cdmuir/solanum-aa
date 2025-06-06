e9e9fcdb876ee7ced60bb82dbeca09d02c4b2a4a
Chris Muir <cdmuir@wisc.edu>
2025-06-06 16:23:13 -0500

    updated loop

--- r/205_fit-model.R
+++ r/205_fit-model.R
@@ -15,7 +15,8 @@ focal_pars = c(
   "sigma_aa_acc_id",
   "b0_log_sigma_aa" ,
   "b_log_sigma_aa_light_intensity_2000",
-  "b_log_sigma_aa_light_treatment_high"
+  "b_log_sigma_aa_light_treatment_high",
+  "nu"
 )
 
 x = 5
@@ -35,29 +36,6 @@ while ((n_divergent > 10 | !converged) & (x < 11)) {
     adapt_delta = ad
   )
   
-  n_divergent = nuts_params(fit_aa1) |>
-    subset(Parameter == "divergent__") |>
-    pull(Value) |>
-    sum()
-  s = fit_aa1$summary(focal_pars)
-  converged = all(s$rhat < 1.01)
-  x = x + 1
-  # x = 2 * x
-  ad = min(ad * 1.1, 0.99)
-  
-  
-  fit_aa5 = aa5$sample(
-    data = stan_data,
-    seed = 987587829 + x,
-    refresh = x * 2000 * 0.1,
-    chains = 4L,
-    parallel_chains = 4L,
-    iter_warmup = x * 1000,
-    iter_sampling = x * 1000,
-    thin = x,
-    adapt_delta = ad
-  )
-  
   n_divergent = nuts_params(fit_aa5) |>
     subset(Parameter == "divergent__") |>
     pull(Value) |>
@@ -65,21 +43,8 @@ while ((n_divergent > 10 | !converged) & (x < 11)) {
   s = fit_aa5$summary(focal_pars)
   converged = all(s$rhat < 1.01)
   x = x + 1
-  # x = 2 * x
   ad = min(ad * 1.1, 0.99)
+  
 }
 
-# if (n_divergent <= 10 & converged) {
-#   fit_aa1$save_object(paste0("objects/fit-aa1/", draw_id, ".rds"))
-# }
-
-log_lik1 <- fit_aa1$draws("log_lik", format = "draws_matrix")
-log_lik5 <- fit_aa5$draws("log_lik", format = "draws_matrix")
-
-loo1 <- loo(log_lik1)
-loo5 <- loo(log_lik5)
-
-loo_compare(loo1, loo5)
-
-tibble()
-fit1 = brm(aa ~ 1, data = stan_data)
+fit_aa5$save_object("objects/fit_aa5.rds")
