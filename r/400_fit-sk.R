344a96715b935bd4c87279b16b0c08e75c9dab2d
cdmuir <cdmuir@wisc.edu>
2025-06-30 07:37:17 -0500

    working on stomatal kinetics stuff

--- r/400_fit-sk.R
+++ r/400_fit-sk.R
@@ -10,7 +10,80 @@ rh_curves = read_rds("data/trimmed_rh_curves.rds") |>
   mutate(ci = as.numeric(as.factor(curve))) |>
   mutate(t_sec = elapsed - min(elapsed), .by = ci)
   
-plan(multisession, workers = 2)
+form_vico = gsw ~ gf + dg * exp(-(t_sec / tau))
+form_cdweibull = gsw ~ gf + dg * exp(-(t_sec / tau) ^ lambda)
+
+bform_vico = bf(
+  form_vico,
+  gf ~ 1,
+  logdg ~ 1,
+  tau ~ 1,
+  nl = TRUE
+)
+
+bform_cdweibull = bf(
+  form_cdweibull,
+  gf ~ 1,
+  logdg ~ 1,
+  tau ~ 1,
+  lambda ~ 1,
+  nl = TRUE
+)
+
+# nls ----
+safe_nls = safely(nls)
+
+nls_summary = rh_curves |>
+  split(~ curve) |>
+  future_imap_dfr(\(df, curve_id) {
+    # df = filter(rh_curves, curve == "LA0107-E_pseudohypo_2000")
+    init = list(gf = min(df$gsw),
+                dg = diff(range(df$gsw)),
+                tau = 50)
+    
+    fit_vico = safe_nls(form_vico, data = df, start = init)
+    
+    if (is.null(fit_vico$result)) {
+      b_vico = tibble(
+        parameter = names(init),
+        value = NA,
+        aic = NA,
+        model = "vico"
+      )
+      init = c(init, lambda = 1)
+    } else {
+      b_vico = as_tibble(coef(fit_vico$result), rownames = "parameter") |>
+        mutate(model = "vico", aic = AIC(fit_vico$result))
+      init = c(as.list(coef(fit_vico$result)), lambda = 1)
+    }
+    
+    fit_cdweibull = safe_nls(form_cdweibull, data = df, start = init)
+    
+    if (is.null(fit_cdweibull$result)) {
+      b_cdweibull = tibble(
+        parameter = names(init),
+        value = NA,
+        aic = NA,
+        model = "cdweibull"
+      )
+    } else {
+      b_cdweibull = as_tibble(coef(fit_cdweibull$result), rownames = "parameter") |>
+        mutate(model = "cdweibull", aic = AIC(fit_cdweibull$result))
+    }
+    
+    bind_rows(b_vico, b_cdweibull) |>
+      mutate(curve = curve_id)
+    
+  }, .progress = TRUE, .options = furrr_options(seed = TRUE))
+
+
+nls_summary |>
+  filter(curve == "LA3778-K_pseudohypo_150")
+
+write_rds(nls_summary, "objects/nls_summary.rds")
+
+# brms ----
+plan(multisession, workers = 9)
 rh_curves |>
   split( ~ curve) |>
   magrittr::extract(seq_len(4)) |>
@@ -18,7 +91,19 @@ rh_curves |>
     file = paste0(sk_dir, curve_id, ".rds")
     if (!file.exists(file)) {
       
-      # set prior?
+      # need to figure out somethign with priors/init
+      # prior1 = prior(normal(1/3, 1/3), nlpar = "gf") +
+      #   prior(normal(-1/4, 1/4), nlpar = "dg", ub = 0) +
+      #   # prior(normal(-1, 1), nlpar = "lambda", ub = 0) +
+      #   prior(normal(100, 100), nlpar = "tau", lb = 0)
+      df = filter(rh_curves, curve == "LA0107-C_pseudohypo_2000")
+      init = list(gf = min(df$gsw),
+                  dg = diff(range(df$gsw)),
+                  tau = 50)
+      fit_vico = nls(form_vico, data = df, start = init)
+      init = c(as.list(coef(fit_vico)), lambda = 1)
+      fit_cdweibull = nls(form_cdweibull, data = df, start = init)
+      AIC(fit_vico, fit_cdweibull)
       
       x = 2
       ad = 0.8
@@ -26,19 +111,13 @@ rh_curves |>
       
       while (n_divergent > 10) {
         fit_curve = brm(
-          bf(
-            gsw ~ gf + dg * exp(-(t_sec / tau)^lambda),
-            gf ~ 1,
-            dg ~ 1,
-            tau ~ 1,
-            lambda ~ 1,
-            nl = TRUE
-          ),
+          formula = fml,
           iter = x * 2000,
           thin = x,
           data = df,
           chains = 1,
           cores = 1,
+          # prior = prior1,
           backend = "cmdstanr",
           control = list(adapt_delta = ad),
           seed = 360036340 + df$ci[1]
@@ -58,9 +137,13 @@ rh_curves |>
   }, .progress = TRUE, .options = furrr_options(seed = TRUE))
 
 
+nls(gsw ~ gf + dg * exp(-(t_sec / tau)),
+    data = df,
+    start = list(gf = 0.3, dg = 0.2, tau = 60)
+)
 
-fit1 = read_rds(paste0(sk_dir, "LA0107-C_pseudohypo_150.rds")) 
-
+fit1 = read_rds(paste0(sk_dir, "LA0107-C_pseudohypo_2000.rds")) 
+plot(fit1$data$t_sec, fit1$data$gsw)
 ce = conditional_effects(fit1) 
 
 ggplot(ce[[1]], aes(t_sec)) +
