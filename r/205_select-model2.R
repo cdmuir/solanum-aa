1963394c21ec6b5005648a4bd8742a1e9b4abece
cdmuir <cdmuir@wisc.edu>
2025-07-06 20:32:31 -0500

    updating select model 2

--- r/205_select-model2.R
+++ r/205_select-model2.R
@@ -31,39 +31,75 @@ random_effects = c(
   "(1 + light_treatment * light_intensity | gr(acc, cov = A))"
 )
 
-sigma_effects = c(
-  "1",
-  "light_treatment",
-  "light_intensity",
-  "light_treatment + light_intensity"
-)
+sigma_effects = c("1",
+                  "light_treatment",
+                  "light_intensity",
+                  "light_treatment + light_intensity")
 
 # Cross all combinations
 set.seed(602275201)
-model_forms = expand.grid(fixed = fixed_effects,
-                          random = random_effects,
-                          sigma = sigma_effects,
-                          stringsAsFactors = FALSE) %>%
-  mutate(seed = sample(1e9, nrow(.)))
+model_forms = expand.grid(
+  fixed = fixed_effects,
+  random = random_effects,
+  sigma = sigma_effects,
+  stringsAsFactors = FALSE
+) %>%
+  mutate(seed = sample(1e9, nrow(.)),
+         model = paste0("model_", row_number()))
 
 # Build and fit each model
 plan(multisession, workers = 19)
 
-aa_models = future_pmap(model_forms, function(fixed, random, sigma, seed) {
-  fml = bf(as.formula(paste("aa ~", fixed, "+", random)), as.formula(paste("sigma ~", sigma)))
-  brm(
-    formula = fml + bf(llma |
-                         mi() ~ light_treatment + (1 + light_treatment |
-                                                     gr(acc, cov = A))),
-    data = d1,
-    data2 = list(A = A),
-    backend = "cmdstanr",
-    chains = 1,
-    family = student(),
-    save_pars = save_pars(all = TRUE),
-    seed = seed
-  )
-}, .progress = TRUE, .options = furrr_options(seed = TRUE))
+aa_models = model_forms |>
+  dplyr::select(fixed, random, sigma, seed) |>
+  future_pmap(function(fixed, random, sigma, seed) {
+    fml = bf(as.formula(paste(
+      "aa | se(se_aa, sigma = TRUE) ~", fixed, "+", random
+    )), as.formula(paste("sigma ~", sigma)))
+    
+    crit = 0
+    x = 1
+    ad = 0.8
+    
+    while (crit == 0 & x < 24) {
+      m = brm(
+        formula = fml + bf(llma |
+                             mi() ~ light_treatment + (1 + light_treatment |
+                                                         gr(acc, cov = A))),
+        data = d1,
+        data2 = list(A = A),
+        backend = "cmdstanr",
+        chains = 1,
+        iter = x * aa_args$n_iter_init,
+        thin = x,
+        family = student(),
+        save_pars = save_pars(all = TRUE),
+        seed = seed
+      )
+      
+      crit = summarise_draws(m) |>
+        summarize(c1 = (max(rhat, na.rm = TRUE) < aa_args$max_rhat),
+                  c2 = (min(ess_bulk, na.rm = TRUE) > aa_args$min_ess)) |>
+        mutate(
+          n_divergent = nuts_params(m) |>
+            subset(Parameter == "divergent__") |>
+            pull(Value) |>
+            sum(),
+          c3 = (n_divergent < aa_args$max_divergent),
+          c4 = c1 * c2 * c3
+        ) |>
+        pull(c4)
+      
+      x = x + 1
+      ad = min(ad * 1.1, 0.99)
+      
+    }
+    
+    return(m)
+    
+  },
+  .progress = TRUE,
+  .options = furrr_options(seed = TRUE))
 
 # Extract loo objects
 loos = future_map(
--- r/207_plot-aa.R
+++ r/207_plot-aa.R
@@ -7,7 +7,8 @@ d1 = fit_aa$data
 df_new = crossing(
   acc = unique(d1$acc),
   light_treatment = c("low", "high"),
-  light_intensity = c("150", "2000")
+  light_intensity = c("150", "2000"),
+  se_aa = 0
 ) |>
   mutate(row = row_number())
 
@@ -40,7 +41,8 @@ df_aa_pred1 = posterior_epred(fit_aa, newdata = df_new) |>
 df_new = crossing(
   acc = first(d1$acc),
   light_treatment = c("low", "high"),
-  light_intensity = c("150", "2000")
+  light_intensity = c("150", "2000"),
+  se_aa = 0
 ) |>
   mutate(row = row_number())
 
@@ -62,7 +64,8 @@ df_aa_pred2 = posterior_epred(fit_aa, newdata = df_new) |>
 df_new = crossing(
   acc = unique(d1$acc),
   light_treatment = c("low", "high"),
-  light_intensity = c("150", "2000")
+  light_intensity = c("150", "2000"),
+  se_aa = 0
 ) |>
   mutate(row = row_number())
 
@@ -132,112 +135,3 @@ ggsave(
 write_rds(df_aa_pred1, "objects/df_aa_pred1.rds")
 write_rds(df_aa_pred2, "objects/df_aa_pred2.rds")
 write_rds(df_aa_pred3, "objects/df_aa_pred3.rds")
-
-# code I am not sure I need
-df_pred2 = posterior_epred(fit_aa, newdata = df_new) |>
-  t() |>
-  as_draws_df() |>
-  mutate(row = row_number()) |>
-  pivot_longer(cols = -row, names_to = "draw", values_to = "aa") |>
-  full_join(df_new, by = "row") |>
-  dplyr::select(-row) |>
-  pivot_wider(names_from = light_treatment, values_from = aa) |>
-  mutate(d_aa = high - low) |>
-  group_by(acc, light_intensity) |>
-  point_interval(d_aa) |>   
-  group_by(light_intensity) |>
-  arrange(d_aa) |>
-  mutate(x = order(d_aa))
-
-ggplot(df_pred2, aes(x, d_aa, ymin = .lower, ymax = .upper, group = acc)) +
-  facet_grid(light_intensity ~ .) +
-  geom_pointinterval() +
-  geom_hline(yintercept = 0, linetype = "dashed") 
-
-df_pred2 |>
-  ungroup() |>
-  dplyr::select(acc, light_intensity, d_aa) |>
-  pivot_wider(
-    names_from = light_intensity,
-    values_from = d_aa
-  ) |>
-  ggplot(aes(`150`, `2000`, label = acc)) +
-  geom_point() +
-  geom_label()
-
-# code for Stan model
-fit_aa = read_rds("objects/fit_aa5.rds")
-
-post1 = fit_aa$draws() |>
-  as_draws_df()
-
-p1 = post1 |>
-  select(
-    starts_with("."),
-    b0_aa,
-    b_aa_light_intensity_2000,
-    b_aa_light_treatment_high,
-    b_aa_2000_high
-  ) |>
-  crossing(
-    light_intensity = c("150", "2000"),
-    light_treatment = c("low", "high"),
-    acc_n = as.character(seq_len(29))
-  )
-
-p2 = post1 |>
-  select(
-    starts_with("."),
-    starts_with("b_aa_light_intensity_2000_acc"),
-    starts_with("b_aa_light_treatment_high_acc"),
-    starts_with("b_aa_2000_high_acc"),
-    matches("^b_aa_acc\\[\\d+\\]$")
-  ) |>
-  pivot_longer(cols = starts_with("b_aa_")) |>
-  mutate(
-    acc_n = str_extract(name, "(?<=\\[)\\d+(?=\\])"),
-    term = str_remove(name, "\\[\\d+\\]"),
-    .keep = "unused"
-  ) |>
-  pivot_wider(
-    names_from = term,
-    values_from = value
-  ) 
-
-p3 = full_join(p1, p2, by = join_by(.chain, .iteration, .draw, acc_n)) |>
-  mutate(b_2000 = b_aa_light_intensity_2000 + b_aa_light_intensity_2000_acc,
-         b_high = b_aa_light_treatment_high + b_aa_light_treatment_high_acc,
-         b_2000_high = b_aa_2000_high + b_aa_2000_high_acc,
-         .keep = "unused") |>
-  mutate(aa = b0_aa + 
-           b_2000 * (light_intensity == "2000") +
-           b_high * (light_treatment == "high") + 
-           b_2000_high * (light_intensity == "2000") * (light_treatment == "high") + b_aa_acc) |>
-  group_by(acc_n, light_intensity, light_treatment) |>
-  point_interval(aa)
-
-ggplot(p3, aes(light_treatment, aa, group = acc_n)) +
-  facet_wrap(~ light_intensity) +
-  geom_line()
-
-p4 = full_join(p1, p2, by = join_by(.chain, .iteration, .draw, acc_n)) |>
-  mutate(b_2000 = b_aa_light_intensity_2000 + b_aa_light_intensity_2000_acc,
-         b_high = b_aa_light_treatment_high + b_aa_light_treatment_high_acc,
-         b_2000_high = b_aa_2000_high + b_aa_2000_high_acc,
-         .keep = "unused") |>
-  mutate(aa = b0_aa + 
-           b_2000 * (light_intensity == "2000") +
-           b_high * (light_treatment == "high") + 
-           b_2000_high * (light_intensity == "2000") * (light_treatment == "high") + b_aa_acc)
-
-p4 |>
-  dplyr::select(.draw, acc_n, light_intensity, light_treatment, aa) |>
-  pivot_wider(
-    names_from = light_treatment,
-    values_from = aa
-  ) |>
-  mutate(d_aa = high - low) |>
-  group_by(acc_n, light_intensity) |>
-  point_interval(d_aa) |>
-  arrange(d_aa) |>
-  print(n = Inf)
