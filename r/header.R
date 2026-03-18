3a37cd54ac3258871c7f225479a73b4fce68fc68
Chris Muir <cdmuir@wisc.edu>
2026-03-18 16:21:13 -0500

    filtered data with bad Tleaf

--- r/header.R
+++ r/header.R
@@ -37,11 +37,11 @@ theme_set(theme_cowplot())
 aa_args = read_rds("objects/aa_args.rds") |>
   c(
     aa_outlier_threshold = 3,
-    thinning_interval = 0.05,
-    n_iter_init = 2e3,
     max_divergent = 10,
     max_rhat = 1.01,
-    min_ess = 400
+    min_ess = 400,
+    n_iter_init = 2e3,
+    thinning_interval = 0.05
   )
 
 write_rds(aa_args, "objects/aa_args1.rds")
